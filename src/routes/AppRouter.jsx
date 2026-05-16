92d939ad62ddf6ad90e0d69b33566d61a0374d37
Bidhya Ghimire <bidhyaghimire55@gmail.com>
2026-05-16 14:14:39 +0545

    made signin component  and added route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
 // import NotFound from "../pages/NotFound";
 
 export default function AppRoutes() {
@@ -8,6 +9,7 @@ export default function AppRoutes() {
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
       </Route>
 
       {/* <Route path="*" element={<NotFound />} /> */}
