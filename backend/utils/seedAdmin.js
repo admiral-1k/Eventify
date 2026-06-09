8e60ef103b30866a1f009bd14c0e2d489d21f034
Bidhya Ghimire <bidhyaghimire55@gmail.com>
2026-06-09 21:31:54 +0545

    added env support for seed admin

--- backend/utils/seedAdmin.js
+++ backend/utils/seedAdmin.js
@@ -4,22 +4,21 @@ const { createUser, getUsersByRole } = require("../model/userModel");
 const seedSuperAdmin = async () => {
   try {
     const superadmins = await getUsersByRole("superadmin");
-    
+
     if (superadmins.length === 0) {
+      const name = process.env.SUPERADMIN_NAME || "Super Admin";
+      const email = process.env.SUPERADMIN_EMAIL || "superadmin@eventify.com";
+      const password = process.env.SUPERADMIN_PASSWORD || "password";
+
       console.log("No superadmin found. Creating default superadmin...");
-      const hashedPassword = await bcrypt.hash("password", 10);
-      await createUser(
-        "Super Admin",
-        "superadmin@eventify.com",
-        hashedPassword,
-        "superadmin"
-      );
-      console.log("Default superadmin created successfully. (Email: superadmin@eventify.com, Password: password)");
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await createUser(name, email, hashedPassword, "superadmin");
+      console.log(`Superadmin created successfully. (Email: ${email})`);
     } else {
       console.log("Superadmin already exists. No need to create.");
     }
   } catch (error) {
-    console.error("Error seeding superadmin:", error);
+    console.error("Error seeding superadmin:", error.message);
   }
 };
 
