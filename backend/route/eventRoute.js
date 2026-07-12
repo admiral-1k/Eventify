fba35a73e833de9ac8bd11e6ccdfd49d8af3a2d3
bhavishlamichhane <bhavishlamichhane@gmail.com>
2026-07-12 16:14:32 +0545

    added code

---
+++ backend/route/eventRoute.js
@@ -0,0 +1,29 @@
+
+
+const express = require("express");
+const router = express.Router();
+
+const {
+  fetchEvents,
+  fetchEvent,
+  addEvent,
+  editEvent,
+  removeEvent,
+} = require("../controller/eventController");
+
+// Get all events
+router.get("/", fetchEvents);
+
+// Get single event
+router.get("/:id", fetchEvent);
+
+// Create event
+router.post("/", addEvent);
+
+// Update event
+router.put("/:id", editEvent);
+
+// Delete event
+router.delete("/:id", removeEvent);
+
+module.exports = router;
